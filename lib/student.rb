c02a366a1d99c49cf2e9e8873c8b19d3d20cf4e3
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 18:58:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,9 +43,9 @@ class Student
   end
   
   def self.create(name, grade)
-    self.new(name, grade)
-    self.save
-    self
+    student = self.new(name, grade)
+    student.save
+    student
   end
 
   # Remember, you can access your database connection anywhere in this class
