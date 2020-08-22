404e1df7733fb33b753ae94daef69882969c1f1b
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 18:53:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     VALUES (?, ?);
     SQL
     
-    DB[:conn].execute(sql, student.name, student.grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   # Remember, you can access your database connection anywhere in this class
