dd00eb2cd9f61409172678774c8b8f14bb3953ac
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 18:55:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn]
   end
 
   # Remember, you can access your database connection anywhere in this class
